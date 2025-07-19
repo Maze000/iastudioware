4f655693de7ac86e4c00630b114497a7972c5ac8
Maze000 <infimo000@gmail.com>
2025-07-19 12:43:38 +1200

    video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,15 +2,33 @@ import { Button } from '@/components/ui/button';
 import { ArrowRight, Play } from 'lucide-react';
 import heroImage from '@/assets/hero-automation.jpg';
 import { Link } from 'react-router-dom';
+import backHero from '@/assets/backHero.mp4';
 const Hero = () => {
   return (
     <section id="home" className="relative min-h-screen flex items-center bg-gradient-dark overflow-hidden pt-[50px] pb-[50px] md:pt-0 md:pb-0">
+      
       {/* Background Effects */}
-      <div className="absolute inset-0 bg-dark-accent">
+      <div className="absolute inset-0 bg-dark-accent z-0">
         <div className="absolute inset-0 bg-gradient-to-br from-primary/10 via-transparent to-secondary/5"></div>
         <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-primary/5 rounded-full blur-3xl"></div>
         <div className="absolute bottom-1/4 right-1/4 w-80 h-80 bg-secondary/5 rounded-full blur-3xl"></div>
       </div>
+
+<video
+  className="absolute inset-0 w-full h-full object-cover z-0"
+  src={backHero}
+  autoPlay
+  loop
+  muted
+  playsInline
+/>
+
+<div className="absolute inset-0 bg-black/60 z-0" />
+
+
+
+
+
 
       <div className="container mx-auto px-4 relative z-10">
         <div className="grid lg:grid-cols-2 gap-12 items-center">
